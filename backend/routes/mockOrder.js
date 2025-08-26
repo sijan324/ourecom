f1b6c7a5028c8325465927b1f8e11942b3a2cb90
Sijan Gautam <sijangautam17@gmail.com>
2025-08-26 20:35:43 +0545

    Fix auth middleware import in mockOrder routes

--- backend/routes/mockOrder.js
+++ backend/routes/mockOrder.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createOrder, getUserOrders, getOrderById, updateOrderStatus } from '../controllers/mockOrderController.js';
-import { authenticateUser } from '../middleware/auth.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
